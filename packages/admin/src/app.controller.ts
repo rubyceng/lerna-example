615fb363a46c016dcba733ca3ab9a3bb7933dcbe
Ruby Ceng <rubyceng0326@gmail.com>
2025-12-31 16:06:52 +0800

    fix: update package prefix name

--- packages/admin/src/app.controller.ts
+++ packages/admin/src/app.controller.ts
@@ -1,5 +1,5 @@
-import * as common from '@lerna-example/common';
 import { Controller, Get } from '@nestjs/common';
+import * as common from '@rubyceng/common';
 
 @Controller()
 export class AppController {
